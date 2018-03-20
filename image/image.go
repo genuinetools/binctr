91b3ab57953bc66611b47061b3e9d9b5e6fc895f
Jess Frazelle <acidburn@microsoft.com>
2018-03-19 22:21:14 -0400

    cleanup
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

---
+++ image/image.go
@@ -0,0 +1,7 @@
+package image
+
+// NAME is the name of the image that is embedded at compile time.
+var NAME string
+
+// SHA is the sha digest of the image that is embedded at compile time.
+var SHA string
