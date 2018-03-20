d65ed3203e212095e55739a0fec22ae309988e59
Jess Frazelle <acidburn@microsoft.com>
2018-03-20 03:02:29 -0400

    update cl-k8s
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- examples/cl-k8s/bindata.go
+++ examples/cl-k8s/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d28ea2d1f67cefed35b70f0b7a314da38b5764dd40b4635d2e475470d1fad65
-size 30784767
+oid sha256:f2e161341cf9636f3d72281432f443cf2772495b7f00bec62affaeaacc695a25
+size 30769935
